20e1ac1333c0ed0cbe122c2df254d31b3de9cb91
Ahmed ALAMI <a.alami@groupeonepoint.com>
2016-06-24 20:59:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var config = require('config');
+var engines = require('engines');
+
+var evaluate = require("./lib/service");
+
+var app = express();
+
+var partials = {
+    layout: 'layout'
+};
+
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+app.engine('html', engines.hogan);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.text({
+    type: 'text/html'
+}));
+
+app.post('/rgaav3/:format/:test', function(request, response) {
+    var content = request.body;
+    var format = request.params.format;
+    var test = request.params.test;
+
+    if(test === "all") {
+      test = null;
+    }
+
+    evaluate(test, content, function(result) {
+        response.setHeader("Content-type", "application/json");
+        response.end(JSON.stringify(result));
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running at localhost:' + app.get('port'));
+});
